5daacb5e65538c293b30497bda1d2748515f15dd
mohammedmoheed <moheed511@gmail.com>
2022-12-21 02:23:48 +0530

    add employee feature added

---
+++ employee/code.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+include('connection.php');
+if(isset($_POST['insert-data'])){
+
+    $name = $_POST['name'];
+    $mobile = $_POST['mobile'];
+    $city = $_POST['city'];
+    $payment = $_POST['payment'];
+
+    $query = "INSERT INTO employee (name,mobile,city,payment) VALUES ('$name', '$mobile', '$city', '$payment' ) ";
+    
+    $query_run = mysqli_query($conn, $query);
+    
+    if($query_run){
+        $_SESSION['status'] = 'inserted succesfull' ;
+        header("location: register_employee.php");
+    }
+    else{
+        $_SESSION['status'] = 'data not inserted ' ;
+        header("location: register_employee.php");
+    }
+
+}
+
+?>
