b62962f2b125c5698c8f322652156d3aba40f45f
mohammedmoheed <moheed511@gmail.com>
2022-12-21 12:09:32 +0530

    not inserting data error solved

--- connection.php
+++ connection.php
@@ -3,11 +3,11 @@ $servername = "localhost";
 $username = "root";
 $password ="";
 $dbname = "smartchoice";
-$conn = new mysqli($servername,$username,$password,$dbname);
+$conn = mysqli_connect($servername,$username,$password,$dbname);
 if($conn->connect_error){
     die("Connection Was Failed".$conn->connect_error);                                     
 }
 else{
-   // echo "Establised";
+   //echo "Establised";
 }
 ?>
--- employee/code.php
+++ employee/code.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-include('connection.php');
+include('../connection.php');
 if(isset($_POST['insert-data'])){
 
     $name = $_POST['name'];
@@ -10,9 +10,9 @@ if(isset($_POST['insert-data'])){
     $city = $_POST['city'];
     $payment = $_POST['payment'];
 
-    $query = "INSERT INTO employee (name,mobile,city,payment) VALUES ('$name', '$mobile', '$city', '$payment' ) ";
+    $query = "INSERT INTO employee (name,mobile,city,payment) VALUES ('$name','$mobile','$city','$payment')";
     
-    $query_run = mysqli_query($conn, $query);
+    $query_run = mysqli_query($conn,$query);
     
     if($query_run){
         $_SESSION['status'] = 'inserted succesfull' ;
